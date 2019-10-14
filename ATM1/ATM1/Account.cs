6c14413390270fd65082900f6afb4e915483294c
Tomasz Grzybowski <grzybson98@gmail.com>
2019-10-15 00:35:37 +0200

    first task

---
+++ ATM1/ATM1/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM1
+{
+    class Account
+    {
+        public float balance;
+        public string pin, number;
+        public Account(float a, string p, string n)
+        {
+            balance = a;
+            pin = p;
+            number = n;
+        }
+        public float getBalance()
+        {
+            return balance;
+        }
+
+        public float withdrawMoney(float money)
+        {
+            balance -= money;
+            return money;
+        }
+
+
+    }
+}
---
+++ ATM1/ATM1/Phone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM1
+{
+    class Phone
+    {
+        public string number;
+        public float balance;
+        public Phone(string n, float b)
+        {
+            number = n;
+            balance = b;
+        }
+
+        public void addBalance(float amount)
+        {
+            balance += amount;
+        }
+
+        public float getBalance()
+        {
+            return balance;
+        }
+    }
+}
